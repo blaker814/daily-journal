ad0c7f2637543a53eb148352eedb3e290802e5f5
Ricky Blake <rab0196@yahoo.com>
2020-08-19 18:15:01 -0500

    automated journal entry additions

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,44 @@
+/*
+ *   Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "08/10/2020",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    },
+    {
+        id: 2,
+        date: "08/12/2020",
+        concept: "GitHub",
+        entry: "We dove into GitHub and learned about it's functionality and work flow.",
+        mood: "Happy"
+    },
+    {
+        id: 3,
+        date: "08/19/2020",
+        concept: "JS Objects",
+        entry: "We learned about JS objects and how to utilize them in creating material for our webpage",
+        mood: "Happy"
+    }
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
