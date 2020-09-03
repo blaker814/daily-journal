de3fc04da5651f9b9403b0077b5235b76845e7c5
Ricky Blake <rab0196@yahoo.com>
2020-09-03 09:35:21 -0500

    set limit on concept field

--- scripts/form/JournalEntryList.js
+++ scripts/form/JournalEntryList.js
@@ -2,6 +2,11 @@ import { useJournalEntries, getJournalEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
 const entryLog = document.querySelector("#entryLog")
+const eventHub = document.querySelector("main")
+
+eventHub.addEventListener("journalStateChanged", event => {
+    
+})
 
 export const EntryListComponent = () => {
     getJournalEntries()
@@ -9,4 +14,8 @@ export const EntryListComponent = () => {
             const entries = useJournalEntries()
             entryLog.innerHTML += entries.map(entry => JournalEntryComponent(entry)).join("")
         })
+    eventHub.addEventListener("journalStateChanged", event => {
+        const entries = useJournalEntries()
+        entryLog.innerHTML += entries.map(entry => JournalEntryComponent(entry)).join("")
+    })
 }
